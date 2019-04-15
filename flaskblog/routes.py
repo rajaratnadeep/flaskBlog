758033b9c4913aa314b819d6f207cb708519cf60
raja <rajaratnadeep@hotmail.com>
2019-04-15 21:12:58 +0530

    vid 8 - Create, Update, and Delete Posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,31 +1,32 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Raja',
-        'title': 'News post 1',
-        'content': 'first post content',
-        'date_posted': 'April 09, 2019'
-    },
-    {
-        'author': 'Ramakanth',
-        'title': 'News post 2',
-        'content': 'second post content',
-        'date_posted': 'April 09, 2019'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Raja',
+#         'title': 'News post 1',
+#         'content': 'first post content',
+#         'date_posted': 'April 09, 2019'
+#     },
+#     {
+#         'author': 'Ramakanth',
+#         'title': 'News post 2',
+#         'content': 'second post content',
+#         'date_posted': 'April 09, 2019'
+#     }
+# ]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -63,7 +64,8 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home')) #ternary operator in python
+            # ternary operator in python
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsucessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -79,16 +81,17 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    
-    #resize the image before saving
-    output_size = (125,125)
+    picture_path = os.path.join(
+        app.root_path, 'static/profile_pics', picture_fn)
+
+    # resize the image before saving
+    output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
-    
-    return picture_fn 
-    #return the file name to user after saving the image file
+
+    return picture_fn
+    # return the file name to user after saving the image file
 
 
 @app.route("/account", methods=['GET', 'POST'])
@@ -102,10 +105,62 @@ def account():
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
-        flash('Your account has been updated!','success')
-        return redirect (url_for('account'))
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static',filename='profile_pics/'+ current_user.image_file)
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data,
+                    content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your message has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!','success')
+        return redirect(url_for('post',post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=[ 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))

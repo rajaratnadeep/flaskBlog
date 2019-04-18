0e428538876a8b007030f788b9489aa7e1510932
raja <rajaratnadeep@hotmail.com>
2019-04-18 07:52:45 +0530

    trial setup done

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
-
 
 class RegistrationForm (FlaskForm):
     username = StringField('Username', validators=[
@@ -57,9 +56,3 @@ class LoginForm (FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content',validators=[DataRequired()])
-    submit = SubmitField('Post')
